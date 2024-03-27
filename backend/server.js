6bc561226fd36c96e9f3eec9a4ad419c865035df
Karan Tushar <karantushar@Karans-MacBook-Air.local>
2024-03-27 09:50:32 -0400

    new changes

--- backend/server.js
+++ backend/server.js
@@ -73,7 +73,7 @@ io.on("connection", (socket) => {
     socket.on("typing", (room) => socket.in(room).emit("typing"));
     socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
 
-    socket.on("new message", (newMessageRecieved) =>
+    socket.on("new message", (newMessageRecieved, room) =>
         // {
         //     var chat = newMessageRecieved.chat;
 
